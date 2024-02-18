8c4a83b423fb14f91ecc80ccfbb814a9e6afc6db
seagull <1k9k1n@gmail.com>
2024-02-17 22:14:17 -0800

    fix enemy rect drawing - pygame.rect.x,y means upper-left

--- enemy.py
+++ enemy.py
@@ -6,14 +6,14 @@ import game_object
 class Enemy(game_object.GameObject):
     def __init__(self):
         self.position = pygame.Vector2(random.randint(0, 640), random.randint(0, 480))
-        self.shape = pygame.Rect(self.position, pygame.Vector2(32, 32))
+        self._size = pygame.Vector2(32, 32)
         self.material = pygame.Color(255, 128, 128)
 
     def update(self, dt):
         pass
 
     def draw(self, screen, camera_position):
-        enemy_view_pos = self.position - camera_position
-        self.shape.x = enemy_view_pos.x
-        self.shape.y = enemy_view_pos.y
-        pygame.draw.rect(screen, self.material, self.shape)
+        min_pos = self.position - self._size / 2
+        enemy_view_pos = min_pos - camera_position
+        rect = pygame.Rect(enemy_view_pos.x, enemy_view_pos.y, self._size.x, self._size.y)
+        pygame.draw.rect(screen, self.material, rect)

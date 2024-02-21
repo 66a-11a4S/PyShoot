377468d16f955fc9d1192c5b86e4c3baa6a8ec75
seagull <1k9k1n@gmail.com>
2024-02-21 15:38:42 -0800

    request add & remove object to its manager

--- game_objects/game_object.py
+++ game_objects/game_object.py
@@ -1,9 +1,18 @@
+from game_objects.game_object_manager import GameObjectManager
+
+
 class GameObject:
     def __init__(self):
         self.enabled = True
+        self._manager = GameObjectManager()
+        self._manager.add(self)
 
     def update(self, dt):
         pass
 
     def draw(self, screen, camera_position):
         pass
+
+    def dispose(self):
+        self.enabled = False
+        self._manager.remove(self)

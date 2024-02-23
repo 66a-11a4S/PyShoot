b5c5d65b8290db32d11751aef002de5725c62685
seagull <1k9k1n@gmail.com>
2024-02-22 18:17:55 -0800

    impl interval func trigger

---
+++ enemy_patterns/interval_pattern.py
@@ -0,0 +1,12 @@
+# 一定時間毎に func を実行する
+class IntervalPattern:
+    def __init__(self, interval, func):
+        self._interval = interval
+        self._timer = 0.0
+        self._func = func
+
+    def update(self, dt):
+        self._timer += dt
+        clamped_timer = min(self._timer, self._interval)
+        self._timer %= self._interval
+        return self._func(clamped_timer)
